ae010bca60c9319bd9597da5e665d3e537c7e602
Mariane Moreira <87588343+marianeleite3@users.noreply.github.com>
2023-08-17 15:42:15 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ kubectl create namespace airflow
 
 kubectl apply -f charts/persistent-volume.yaml
 kubectl apply -f charts/persistent-volume-claim.yaml
-helm upgrade --install airflow apache-airflow/airflow -n airflow -f charts/values.yaml --version 1.9.0 --debug
+helm upgrade --install airflow apache-airflow/airflow -n airflow --values charts/values.yaml --version v1.9.0 --debug
 
 kubectl port-forward svc/airflow-webserver 8080:8080 -n airflow
