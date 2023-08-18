a038348a7ae1484205a00bcbe8567f97157de0ad
Mariane Moreira <87588343+marianeleite3@users.noreply.github.com>
2023-08-18 17:13:23 -0300

    Add files via upload

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,7 @@ helm repo add apache-airflow https://airflow.apache.org
 helm repo update
 kubectl create namespace airflow
 
-kubectl apply -f charts/persistent-volume.yaml
-kubectl apply -f charts/persistent-volume-claim.yaml
-helm upgrade --install airflow apache-airflow/airflow -n airflow --values charts/values.yaml --version v1.9.0 --debug
+helm install airflow apache-airflow/airflow -f charts/values.yaml --namespace airflow
 
 kubectl port-forward svc/airflow-webserver 8080:8080 -n airflow
+
