9b1adf06b269072b27b480f9f54ec2390ee4fc93
Mariane Moreira <87588343+marianeleite3@users.noreply.github.com>
2023-08-21 22:48:34 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ helm repo update
 
 helm upgrade --install airflow apache-airflow/airflow --namespace airflow --create-namespace --timeout 10m
 
-helm upgrade --install airflow apache-airflow/airflow -n airflow -f carts/values.yaml
+helm upgrade --install airflow apache-airflow/airflow -n airflow -f charts/values.yaml
 
 kubectl port-forward svc/airflow-web 8080:8080 -n airflow --address 0.0.0.0 
