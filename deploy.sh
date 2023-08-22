e9edd6b6310104096fb3826b4226464b0812c481
Mariane Moreira <87588343+marianeleite3@users.noreply.github.com>
2023-08-21 22:41:55 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 helm repo add apache-airflow https://airflow.apache.org
 helm repo update
-kubectl create namespace airflow
 
-helm install airflow apache-airflow/airflow -f charts/values.yaml --namespace airflow
+helm upgrade --install airflow apache-airflow/airflow --namespace airflow --create-namespace --timeout 10m
 
-kubectl port-forward svc/airflow-webserver 8080:8080 -n airflow
+helm upgrade --install airflow apache-airflow/airflow -n airflow -f values.yaml
 
+kubectl port-forward svc/airflow-web 8080:8080 -n airflow --address 0.0.0.0 
